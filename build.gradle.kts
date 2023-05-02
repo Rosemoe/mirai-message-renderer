551e9064d403ef727158278daa98083e57ebffdf
Rosemoe <2073412493@qq.com>
2023-05-02 22:21:12 +0800

    fix(deps): update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    val kotlinVersion = "1.6.21"
+    val kotlinVersion = "1.8.20"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
 
-    id("net.mamoe.mirai-console") version "2.11.1"
+    id("net.mamoe.mirai-console") version "2.14.0"
 }
 
 group = "io.github.rosemoe"
@@ -17,7 +17,7 @@ repositories {
 }
 
 dependencies {
-    val VERSION = "2.11.1"
+    val VERSION = "2.14.0"
     implementation("net.mamoe:mirai-core:$VERSION") // mirai-core 的 API
     implementation("net.mamoe:mirai-console:$VERSION") // 后端
     testImplementation(kotlin("test"))
